34a8d77238b8cb54255a98090a3d7631c9cb224e
John Marcao <john.marcao@gmail.com>
2023-03-06 21:09:11 -0500

    Change to simpler Jekyll base

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "DroneDAD",
+        "excerpt":"DroneDAD is an IoT device designed from the ground up for ESE516 IoT Edge Computing at University of Pennsylvania. I worked with my partner Riddhi Doshi to go through the entire development lifecylce of the device. Requirements and Design Component Selection Schematic Design and PCB Layout Manufacturing Testing LOTS of...","categories": ["Projects"],
+        "tags": [],
+        "url": "/projects/dronedad/",
+        "teaser": null
+      },{
+        "title": "CUDA Pathtracer",
+        "excerpt":"Github Link Contents Introduction to Pathtracing Pathtracer Implementation Visual Effects Ideal Diffuse Scattering Function Imperfect Specular Reflective Scattering Function Refractive Transmission Scattering Function Fresnel Effect Antialiasing Depth of Field Motion Blur glTF Objects Performance Improvements Compaction of Terminated Rays First-Bounce Caching Material Sorting Performance Comparison Bloopers Acknowledgements Introduction to Pathtracing...","categories": ["Projects"],
+        "tags": [],
+        "url": "/projects/cuda-pathtracer/",
+        "teaser": null
+      }]
